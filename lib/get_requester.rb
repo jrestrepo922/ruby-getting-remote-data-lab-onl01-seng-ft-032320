12f757f3a4c50a0baaeabd3d3a6d3cf57b1c4dfa
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-10 19:46:29 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,12 +7,13 @@ class GetRequester
 
   def initialize(url)
     @url = url
-
   end
 
 
   def get_response_body
-
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
 
 
